1c8b390781da8fbd6e97aa04ea6152b614dca0b5
Caius Durling <dev@caius.name>
2022-02-13 09:50:12 +0000

    nix flakes for all systems

--- flake.nix
+++ flake.nix
@@ -4,16 +4,20 @@
   inputs = {
     # Contains hugo 0.92.1
     nixpkgs.url = "github:NixOS/nixpkgs/04e546b93148b4c31026a14fb3edae876816105e";
+
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-  in {
-    devShell.aarch64-darwin = pkgs.mkShell {
-      buildInputs = [
-        pkgs.hugo
-      ];
-    };
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in rec {
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          pkgs.hugo
+        ];
+      };
+    }
+  );
 }
